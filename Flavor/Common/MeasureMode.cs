4272d3709af8d0d5da2115ee834ac6738b775350
dmitrii.lebedev <deathlust@users.noreply.github.com>
2011-03-29 17:21:21 +0000

    Stub for new Monitor implementation.

--- Flavor/Common/MeasureMode.cs
+++ Flavor/Common/MeasureMode.cs
@@ -400,6 +400,157 @@ namespace Flavor.Common {
                     return base.stepsCount() * stopperTurns;
                 }
             }
+            internal class Monitor1: MeasureMode {
+                private class MeasureStopper {
+                    private int counter;
+                    private Timer timer;
+                    private bool timerElapsed = false;
+                    internal MeasureStopper(int counterLimit, int timeLimit) {
+                        counter = counterLimit == 0 ? -1 : counterLimit;
+                        if (timeLimit > 0) {
+                            timer = new Timer();
+                            // time in minutes
+                            timer.Interval = timeLimit * 60000;
+                            timer.AutoReset = false;
+                            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+                        }
+                    }
+
+                    void timer_Elapsed(object sender, ElapsedEventArgs e) {
+                        timerElapsed = true;
+                        timer.Elapsed -= new ElapsedEventHandler(timer_Elapsed);
+                    }
+                    internal void next() {
+                        if (counter == -1) {
+                            // produce infinite loop
+                            return;
+                        }
+                        --counter;
+                    }
+                    internal bool ready() {
+                        return timerElapsed || counter == 0;
+                    }
+                    internal int estimatedTurns() {
+                        // estimated operation duration
+                        return counter;
+                    }
+
+                    internal void startTimer() {
+                        if (timer == null) {
+                            return;
+                        }
+                        timer.Start();
+                    }
+                }
+
+                private MeasureStopper stopper;
+                private Utility.PreciseEditorData peak;
+                private bool spectrumIsValid = true;
+                private ushort allowedShift;
+
+                private Precise preciseCycle;
+                private short shift = 0;
+
+                internal Monitor1(short initialShift, ushort allowedShift, int timeLimit) {
+                    preciseCycle = new Precise(Config.PreciseDataWithChecker);
+                    shift = initialShift;
+                    this.allowedShift = allowedShift;
+                    stopper = new MeasureStopper(Config.Iterations, timeLimit);
+                    peak = Config.CheckerPeak;
+                }
+                protected override void onSuccessfulExit() {
+                    //TODO: option-dependent behaviour: drop or save data on shift situation. See similar comment in toContinue()
+                    if (true && spectrumIsValid) {
+                        preciseCycle.onSuccessfulExit();
+                    }
+                }
+                protected override void finalize() {
+                    Config.finalizeMonitorFile();
+                }
+                protected override void saveData() {
+                    // senseModeCounts here?
+                    Config.autoSaveMonitorSpectrumFile(shift);
+                }
+
+                protected override bool onNextStep() {
+                    int realValue = pointValue + shift;
+                    if (realValue > Config.MAX_STEP || realValue < Config.MIN_STEP) {
+                        return false;
+                    }
+                    Commander.AddToSend(new sendSVoltage((ushort)realValue));
+                    ++pointValue;
+                    return true;
+                }
+                protected override bool toContinue() {
+                    if (preciseCycle.toContinue()) {
+                        return true;
+                    }
+                    //TODO: option-dependent behaviour: transition to next cycle on shift situation. See similar comment in onSuccessfulExit()
+                    if (true && spectrumIsValid) {
+                        stopper.next();
+                    }
+                    if (stopper.ready()) {
+                        return false;
+                    }
+                    // operations between iterations
+                    onSuccessfulExit();
+                    preciseCycle.init(true);
+                    return true;
+                }
+                private bool isSpectrumValid(Utility.PreciseEditorData curPeak) {
+                    if (!curPeak.Equals(peak)) {
+                        // do not store value here!
+                        return true;
+                    }
+                    long[] counts = preciseCycle.peakCounts(isCheckPeak);
+                    ushort width = peak.Width;
+                    if (counts.Length != 2 * width + 1) {
+                        // data mismatch
+                        return spectrumIsValid = false;
+                    }
+                    long max = -1;
+                    int index = -1;
+                    for (int i = 0; i < counts.Length; ++i) {
+                        long temp = counts[i];
+                        if (temp > max) {
+                            max = temp;
+                            index = i;
+                        }
+                    }
+
+                    short delta = (short)(index - width);
+                    if (delta > allowedShift || delta < -allowedShift) {
+                        shift += delta;
+                        return spectrumIsValid = false;
+                    }
+                    return spectrumIsValid = true;
+                }
+                private bool isCheckPeak(Utility.PreciseEditorData ped) {
+                    // only in this situation (checkpeak marked with false use)
+                    if (ped.Use) {
+                        return false;
+                    }
+                    return true;
+                }
+                internal override bool start() {
+                    if (!base.start()) {
+                        return false;
+                    }
+                    stopper.startTimer();
+                    return true;
+                }
+                internal override void updateGraph() {
+                    ushort pnt = pointValue;
+                    Graph.updateGraphDuringPreciseMeasure(--pnt, preciseCycle.SenseModePeak);
+                }
+                internal override int stepsCount() {
+                    int stopperTurns = stopper.estimatedTurns();
+                    if (stopperTurns <= 0) {
+                        return 0;
+                    }
+                    return preciseCycle.stepsCount() * stopperTurns;
+                }
+            }
         }
     }
 }
