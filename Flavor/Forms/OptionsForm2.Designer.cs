f393bccc1b7514ef2365c976ab4d145a729b0864
dmitrii.lebedev <deathlust@users.noreply.github.com>
2014-05-29 13:29:27 +0000

    Added stub for modified options dialog.

---
+++ Flavor/Forms/OptionsForm2.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Flavor.Forms {
+    partial class OptionsForm2 {
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.Button loadFileButton;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsForm2));
+            System.Windows.Forms.Button saveFileButton;
+            this.params_groupBox = new System.Windows.Forms.GroupBox();
+            this.adjustSettingsCheckBox = new System.Windows.Forms.CheckBox();
+            this.ok_butt = new System.Windows.Forms.Button();
+            this.cancel_butt = new System.Windows.Forms.Button();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.rareModeCheckBox = new System.Windows.Forms.CheckBox();
+            this.openCommonDataFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveCommonDataFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            loadFileButton = new System.Windows.Forms.Button();
+            saveFileButton = new System.Windows.Forms.Button();
+            this.params_groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadFileButton
+            // 
+            resources.ApplyResources(loadFileButton, "loadFileButton");
+            loadFileButton.Name = "loadFileButton";
+            loadFileButton.UseVisualStyleBackColor = true;
+            loadFileButton.Click += new System.EventHandler(this.loadFileButton_Click);
+            // 
+            // saveFileButton
+            // 
+            resources.ApplyResources(saveFileButton, "saveFileButton");
+            saveFileButton.Name = "saveFileButton";
+            saveFileButton.UseVisualStyleBackColor = true;
+            saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            // 
+            // params_groupBox
+            // 
+            this.params_groupBox.Controls.Add(this.treeView1);
+            this.params_groupBox.Controls.Add(this.adjustSettingsCheckBox);
+            this.params_groupBox.Controls.Add(loadFileButton);
+            this.params_groupBox.Controls.Add(saveFileButton);
+            resources.ApplyResources(this.params_groupBox, "params_groupBox");
+            this.params_groupBox.Name = "params_groupBox";
+            this.params_groupBox.TabStop = false;
+            // 
+            // adjustSettingsCheckBox
+            // 
+            resources.ApplyResources(this.adjustSettingsCheckBox, "adjustSettingsCheckBox");
+            this.adjustSettingsCheckBox.Name = "adjustSettingsCheckBox";
+            this.adjustSettingsCheckBox.UseVisualStyleBackColor = true;
+            this.adjustSettingsCheckBox.CheckedChanged += new System.EventHandler(this.adjustSettingsCheckBox_CheckedChanged);
+            // 
+            // ok_butt
+            // 
+            resources.ApplyResources(this.ok_butt, "ok_butt");
+            this.ok_butt.Name = "ok_butt";
+            this.ok_butt.UseVisualStyleBackColor = true;
+            this.ok_butt.Click += new System.EventHandler(this.ok_butt_Click);
+            // 
+            // cancel_butt
+            // 
+            this.cancel_butt.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.cancel_butt, "cancel_butt");
+            this.cancel_butt.Name = "cancel_butt";
+            this.cancel_butt.UseVisualStyleBackColor = true;
+            this.cancel_butt.Click += new System.EventHandler(this.cancel_butt_Click);
+            // 
+            // applyButton
+            // 
+            resources.ApplyResources(this.applyButton, "applyButton");
+            this.applyButton.Name = "applyButton";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+            // 
+            // rareModeCheckBox
+            // 
+            resources.ApplyResources(this.rareModeCheckBox, "rareModeCheckBox");
+            this.rareModeCheckBox.Name = "rareModeCheckBox";
+            this.rareModeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // openCommonDataFileDialog
+            // 
+            this.openCommonDataFileDialog.DefaultExt = "cdf";
+            resources.ApplyResources(this.openCommonDataFileDialog, "openCommonDataFileDialog");
+            // 
+            // saveCommonDataFileDialog
+            // 
+            this.saveCommonDataFileDialog.DefaultExt = "cdf";
+            resources.ApplyResources(this.saveCommonDataFileDialog, "saveCommonDataFileDialog");
+            // 
+            // treeView1
+            // 
+            resources.ApplyResources(this.treeView1, "treeView1");
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            ((System.Windows.Forms.TreeNode)(resources.GetObject("treeView1.Nodes"))),
+            ((System.Windows.Forms.TreeNode)(resources.GetObject("treeView1.Nodes1")))});
+            // 
+            // OptionsForm2
+            // 
+            this.AcceptButton = this.ok_butt;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel_butt;
+            this.Controls.Add(this.rareModeCheckBox);
+            this.Controls.Add(this.applyButton);
+            this.Controls.Add(this.cancel_butt);
+            this.Controls.Add(this.ok_butt);
+            this.Controls.Add(this.params_groupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OptionsForm2";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.params_groupBox.ResumeLayout(false);
+            this.params_groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        protected System.Windows.Forms.OpenFileDialog openCommonDataFileDialog;
+        protected System.Windows.Forms.SaveFileDialog saveCommonDataFileDialog;
+        protected System.Windows.Forms.Button cancel_butt;
+        protected System.Windows.Forms.Button applyButton;
+        protected System.Windows.Forms.Button ok_butt;
+        protected System.Windows.Forms.GroupBox params_groupBox;
+        private System.Windows.Forms.CheckBox adjustSettingsCheckBox;
+        protected System.Windows.Forms.CheckBox rareModeCheckBox;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ Flavor/Forms/OptionsForm2.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+using Config = Flavor.Common.Config;
+using CommonOptions = Flavor.Common.CommonOptions;
+// really here?
+using ProgramStates = Flavor.Common.ProgramStates;
+using ProgramEventHandler = Flavor.Common.ProgramEventHandler;
+
+namespace Flavor.Forms {
+    partial class OptionsForm2: Form {
+        protected OptionsForm2() {
+            InitializeComponent();
+        }
+
+        void loadCommonData(CommonOptions co) {
+            // TODO: remove hard-coded numbers here (use constants in Config)
+            //setupNumericUpDown(expTimeNumericUpDown, 10, 10000, co.eTimeReal);
+            //setupNumericUpDown(idleTimeNumericUpDown, 10, 100, co.iTimeReal);
+
+            //setupNumericUpDown(iVoltageNumericUpDown, 20, 150, co.iVoltageReal, CommonOptions.iVoltageConvert, CommonOptions.iVoltageConvert);
+            //setupNumericUpDown(CPNumericUpDown, 10, 12, co.CPReal, CommonOptions.CPConvert, CommonOptions.CPConvert);
+            //setupNumericUpDown(eCurrentNumericUpDown, 0, 10, co.eCurrentReal, CommonOptions.eCurrentConvert, CommonOptions.eCurrentConvert);
+            //setupNumericUpDown(hCurrentNumericUpDown, 0, 1, co.hCurrentReal, CommonOptions.hCurrentConvert, CommonOptions.hCurrentConvert);
+            //setupNumericUpDown(fV1NumericUpDown, 20, 150, co.fV1Real, CommonOptions.fV1Convert, CommonOptions.fV1Convert);
+            //setupNumericUpDown(fV2NumericUpDown, 20, 150, co.fV2Real, CommonOptions.fV2Convert, CommonOptions.fV2Convert);
+        }
+        void setupNumericUpDown(NumericUpDown updown, decimal min, decimal max, double value) {
+            updown.Minimum = min;
+            updown.Maximum = max;
+            decimal temp = (decimal)value;
+            if (temp < updown.Minimum)
+                temp = updown.Minimum;
+            else if (temp > updown.Maximum)
+                temp = updown.Maximum;
+            updown.Value = temp;
+        }
+        delegate ushort ConvertTo(double value);
+        delegate double ConvertFro(ushort value);
+        void setupNumericUpDown(NumericUpDown updown, double min, double max, double value, ConvertTo conv1, ConvertFro conv2) {
+            setupNumericUpDown(updown, (decimal)conv2(conv1(min)), (decimal)conv2(conv1(max)), value);
+        }
+
+        protected void cancel_butt_Click(object sender, EventArgs e) {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        protected virtual void ok_butt_Click(object sender, EventArgs e) {
+            //Config.saveGlobalCommonOptions(
+            //    (ushort)expTimeNumericUpDown.Value,
+            //    (ushort)idleTimeNumericUpDown.Value,
+            //    (double)iVoltageNumericUpDown.Value,
+            //    (double)CPNumericUpDown.Value,
+            //    (double)eCurrentNumericUpDown.Value,
+            //    (double)hCurrentNumericUpDown.Value,
+            //    (double)fV1NumericUpDown.Value,
+            //    (double)fV2NumericUpDown.Value);
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        protected virtual void applyButton_Click(object sender, EventArgs e) {
+            //Config.saveGlobalCommonOptions(
+            //    (ushort)expTimeNumericUpDown.Value,
+            //    (ushort)idleTimeNumericUpDown.Value,
+            //    (double)iVoltageNumericUpDown.Value,
+            //    (double)CPNumericUpDown.Value,
+            //    (double)eCurrentNumericUpDown.Value,
+            //    (double)hCurrentNumericUpDown.Value,
+            //    (double)fV1NumericUpDown.Value,
+            //    (double)fV2NumericUpDown.Value);
+            this.DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        protected void saveFileButton_Click(object sender, EventArgs e) {
+            if (saveCommonDataFileDialog.ShowDialog() == DialogResult.OK) {
+                //Config.saveCommonOptions(saveCommonDataFileDialog.FileName, (ushort)expTimeNumericUpDown.Value, (ushort)idleTimeNumericUpDown.Value,
+                //                         (double)iVoltageNumericUpDown.Value, (double)CPNumericUpDown.Value, (double)eCurrentNumericUpDown.Value, (double)hCurrentNumericUpDown.Value,
+                //                         (double)fV1NumericUpDown.Value, (double)fV2NumericUpDown.Value);
+            }
+        }
+
+        protected void loadFileButton_Click(object sender, EventArgs e) {
+            if (openCommonDataFileDialog.ShowDialog() == DialogResult.OK) {
+                try {
+                    loadCommonData(Config.loadCommonOptions(openCommonDataFileDialog.FileName));
+                } catch (Config.ConfigLoadException cle) {
+                    cle.visualise();
+                }
+            }
+        }
+
+        void adjustSettingsCheckBox_CheckedChanged(object sender, EventArgs e) {
+            //CPNumericUpDown.ReadOnly = !adjustSettingsCheckBox.Checked;
+            //fV1NumericUpDown.ReadOnly = !adjustSettingsCheckBox.Checked;
+            //fV2NumericUpDown.ReadOnly = !adjustSettingsCheckBox.Checked;
+            //hCurrentNumericUpDown.ReadOnly = !adjustSettingsCheckBox.Checked;
+        }
+        void InvokeSetVisibility(ProgramStates state) {
+            this.Invoke(new Action(() => {
+                // TODO: avoid bringing to front..
+                this.Visible = state != ProgramStates.Measure ||
+                    state != ProgramStates.BackgroundMeasureReady ||
+                    state != ProgramStates.WaitBackgroundMeasure;
+            }));
+        }
+        public class LoadEventArgs: EventArgs {
+            public bool Enabled { get; set; }
+            public bool NotRareModeRequested { get; set; }
+            public ProgramEventHandler Method { get; set; }
+        }
+        protected override void OnLoad(EventArgs e) {
+            var args = e is LoadEventArgs ? e as LoadEventArgs : new LoadEventArgs();
+            args.Method += InvokeSetVisibility;
+            base.OnLoad(args);
+            rareModeCheckBox.Checked = args.NotRareModeRequested;
+            if (args.Enabled) {
+                applyButton.Enabled = true;
+                applyButton.Visible = true;
+            } else {
+                applyButton.Enabled = false;
+                applyButton.Visible = false;
+            }
+            CommonOptions co = Config.CommonOptions;
+            if (co != null)
+                loadCommonData(Config.CommonOptions);
+        }
+        public class ClosingEventArgs: FormClosingEventArgs {
+            public bool NotRareModeRequested { get; set; }
+            public ProgramEventHandler Method { get; set; }
+            public ClosingEventArgs(FormClosingEventArgs args)
+                : base(args.CloseReason, args.Cancel) { }
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            var args = e is ClosingEventArgs ? e as ClosingEventArgs : new ClosingEventArgs(e);
+            args.NotRareModeRequested = rareModeCheckBox.Checked;
+            args.Method += InvokeSetVisibility;
+            base.OnFormClosing(args);
+        }
+    }
+}
